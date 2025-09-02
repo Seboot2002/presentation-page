5f72a0236594da8bd054f81184a730b856511857
Seboot2002 <sebas.camayo2002@gmail.com>
2025-09-02 16:57:10 -0500

    Cambios en los links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { HomeComponent } from './features/home/home.component';
 import { MultiplayerGameComponent } from './features/multiplayer-game/multiplayer-game.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', children: [
+  {path: '', children: [
     {path: '', component: HomeComponent},
     {path: 'multiplayer-game', component: MultiplayerGameComponent},
   ]},
